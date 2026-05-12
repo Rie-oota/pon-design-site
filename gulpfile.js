5a0cb4fd142281361d3ee63908adddabea01ec64
Yoshida <sunnri257@gmail.com>
2026-05-12 11:49:46 +0900

    fix: layout and README

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,17 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
-
-gulp.task("default", function () {
-  return gulp.watch("scss/**/*.scss", function () {
-    return gulp.src("scss/style.scss")
-      .pipe(
-        sass({
-          outputStyle: "expanded"
-        }).on("error", sass.logError)
-      )
-      .pipe(gulp.dest("css"));
-  });
-});
-
 const nunjucks = require("gulp-nunjucks-html");
+
+function compileSass() {
+  return gulp
+    .src("scss/style.scss")
+    .pipe(
+      sass({
+        outputStyle: "expanded"
+      }).on("error", sass.logError)
+    )
+    .pipe(gulp.dest("css"));
+}
 
 function html() {
   return gulp
@@ -28,8 +26,13 @@ function html() {
 }
 
 function watchFiles() {
+  gulp.watch("scss/**/*.scss", compileSass);
   gulp.watch("src/**/*.njk", html);
 }
 
+exports.sass = compileSass;
 exports.html = html;
-exports.default = gulp.series(html, watchFiles);
+exports.default = gulp.series(
+  gulp.parallel(compileSass, html),
+  watchFiles
+);
